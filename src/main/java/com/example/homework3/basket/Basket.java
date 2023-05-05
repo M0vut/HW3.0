3910cf0840a9b56f150c336735c4f5cd75dce7a0
hp <momti@mail.ru>
2023-05-05 16:25:16 +0300

    Домашнее задание 3.0

---
+++ src/main/java/com/example/homework3/basket/Basket.java
@@ -0,0 +1,18 @@
+package com.example.homework3.basket;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.context.annotation.SessionScope;
+
+import java.util.ArrayList;
+import java.util.List;
+@Component
+@SessionScope
+public class Basket {
+  private   List<Integer>firstBasket = new ArrayList<>();
+  public void addAll(List<Integer>values){
+      firstBasket.addAll(values);
+  }
+  public List<Integer> getAll(){
+      return firstBasket;
+    }
+}
